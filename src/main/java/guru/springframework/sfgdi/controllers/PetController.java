d471b8d3f15ede055e5d0689d5d10d0746a7b360
huntum <mohit.jagtiani@sytac.io>
2020-05-06 23:56:06 +0200

    assignment 4 completed

--- src/main/java/guru/springframework/sfgdi/controllers/PetController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PetController.java
@@ -1,6 +1,8 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.PetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -11,7 +13,7 @@ public class PetController {
 
     private final PetService petService;
 
-    public PetController(PetService petService) {
+    public PetController(@Qualifier("petService") PetService petService) {
         this.petService = petService;
     }
 
